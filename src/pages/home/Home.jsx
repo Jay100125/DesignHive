130ada2310f4968e4aba7938347ef0d53f208258
jeet <jeetp105520@gmail.com>
2023-09-16 12:26:18 +0530

    making slider

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import "./Home.scss"
+import Featured from '../../components/featured/Featured'
+import TrustedBy from '../../components/trustedby/TrustedBy'
+import Slide from '../../components/slide/Slide'
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Featured/>
+      <TrustedBy/>
+      <Slide/>
+    </div>
   )
 }
 
